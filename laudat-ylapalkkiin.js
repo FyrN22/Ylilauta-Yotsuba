4f217713a3569249252d250d7ef311a271b28e2f
eeeee22222 <118836819+FyrN22@users.noreply.github.com>
2024-02-17 17:49:25 +0000

    Update laudat-ylapalkkiin.js

--- laudat-ylapalkkiin.js
+++ laudat-ylapalkkiin.js
@@ -1,4 +1,5 @@
 // ==UserScript==
+// ==UserScript==
 // @name        laudat-yläpalkkiin
 // @namespace   Violentmonkey Scripts
 // @match       https://ylilauta.org/*
@@ -14,6 +15,7 @@ function createButton(text, url, className) {
     var button = document.createElement('button');
     button.innerText = text;
     button.classList.add('nappi');
+    button.classList.add(className);
     button.addEventListener('click', function() {
         window.location.href = url;
     });
@@ -23,18 +25,31 @@ function createButton(text, url, className) {
 var buttonContainer = document.createElement('div');
 buttonContainer.classList.add('navbar-buttons');
 
-buttonContainer.appendChild(createButton("[ ukko /", "https://ylilauta.org/thread/", "navbar-button"));
-buttonContainer.appendChild(createButton("b /", "https://ylilauta.org/satunnainen/", "navbar-button"));
-buttonContainer.appendChild(createButton("m /", "https://ylilauta.org/masiinat/", "navbar-button"));
-buttonContainer.appendChild(createButton("tubetus /", "https://ylilauta.org/tubetus/", "navbar-button"));
-buttonContainer.appendChild(createButton("n /", "https://ylilauta.org/ninja/", "navbar-button"));
-buttonContainer.appendChild(createButton("fap /", "https://ylilauta.org/kasityot/", "navbar-button"));
-buttonContainer.appendChild(createButton("hikky /", "https://ylilauta.org/hikky/", "navbar-button"));
-buttonContainer.appendChild(createButton("g /", "https://ylilauta.org/pelit/", "navbar-button"));
-buttonContainer.appendChild(createButton("h /", "https://ylilauta.org/huumeet/", "navbar-button"));
-buttonContainer.appendChild(createButton("epic ]", "https://ylilauta.org/arkisto/", "navbar-button"));
-
-
+buttonContainer.appendChild(createButton("[", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("ukko", "https://ylilauta.org/thread/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("b", "https://ylilauta.org/satunnainen/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("m", "https://ylilauta.org/masiinat/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("tubetus", "https://ylilauta.org/tubetus/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("n", "https://ylilauta.org/ninja/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("fap", "https://ylilauta.org/kasityot/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("hikky", "https://ylilauta.org/hikky/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("g", "https://ylilauta.org/pelit/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("h", "https://ylilauta.org/huumeet/", "navbar-button"));
+buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+buttonContainer.appendChild(createButton("epic", "https://ylilauta.org/arkisto/", "navbar-button"));
+buttonContainer.appendChild(createButton("]", "navbar-button", "merkki"));
 
 navbar.insertBefore(buttonContainer, navbar.children[1]);
-navbar.insertBefore(createButton("[ Asetukset /", "https://ylilauta.org/user/", "navbar-button"), navbar.children[3]);
+navbar.insertBefore(createButton("[", "navbar-button", "merkki"), navbar.children[3]);
+navbar.insertBefore(createButton("Asetukset", "https://ylilauta.org/user/", "navbar-button"), navbar.children[4]);
+navbar.insertBefore(createButton("/", "navbar-button", "merkki"), navbar.children[5]);
+
+r.children[3]);
