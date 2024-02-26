b4e25dc019b9795b4b967f9a47652d8765f9f997
eeeee22222 <118836819+FyrN22@users.noreply.github.com>
2024-02-26 21:48:03 +0200

    Update laudat-ylapalkkiin.js

--- laudat-ylapalkkiin.js
+++ laudat-ylapalkkiin.js
@@ -15,36 +15,82 @@ function createButton(text, url, className) {
     button.innerText = text;
     button.classList.add('nappi');
     button.classList.add(className);
-    button.addEventListener('click', function() {
-        window.location.href = url;
+    button.addEventListener('click', function(event) {
+        if (text === "/" || text === "[" || text === "]") {
+            event.preventDefault();
+        } else {
+            window.location.href = url;
+        }
     });
     return button;
 }
+
+const boardAbbreviations = {
+    "satunnainen": "b",
+    "masiinat": "m",
+    "pelit": "g",
+    "bigbrother": "bb",
+    "kasityot": "fap",
+    "international": "int",
+    "taide": "art",
+    "televisio": "tv",
+    "huumeet": "h",
+    "matkustus": "coco",
+    "paranormaali": "x",
+    "juorut": "j",
+    "videot": "mp4",
+    "rikokset": "r",
+    "tubetus": "yt",
+    "uutiset": "u",
+    "suhteet": "sopo",
+    "opiskelu": "o",
+    "terveys": "t",
+    "harrastukset": "h",
+    "kirjallisuus": "k",
+    "arkisto": "epic",
+    "luonto": "l",
+    "nikotiinipussit": "niksa",
+    "penkkiurheilu": "pu",
+    "tekoaly": "ai",
+    "ruokajajuoma": "nom",
+    "uhkapelit": "win",
+    "aihevapaa": "av",
+    "akuankka": "aku",
+    "kryptovaluutat": "crypto",
+    "ohjelmointi": "dev",
+    "ajoneuvot":  "auto",
+    "yhteiskunta": "pol",
+    "joulukalenteri": "joulu",
+    "bilderberg": "roll",
+    "tiede": "sci",
+    "ninja": "n",
+    "koronavirus": "covid",
+    "homoseksuaalisuus": "fag",
+};
+
+const hidden = ["koronavirus", "homoseksuaalisuus", "turri", "hisparefugio", "lusofono", "casinofoorumi", "meemivala", "de", "tyo", "uskonnot", "seksuaalisuus", "mielenterveys"];
 
 var buttonContainer = document.createElement('div');
 buttonContainer.classList.add('navbar-buttons');
 
 buttonContainer.appendChild(createButton("[", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("ukko", "https://ylilauta.org/thread/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("b", "https://ylilauta.org/satunnainen/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("m", "https://ylilauta.org/masiinat/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("tubetus", "https://ylilauta.org/tubetus/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("n", "https://ylilauta.org/ninja/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("fap", "https://ylilauta.org/kasityot/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("hikky", "https://ylilauta.org/hikky/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("g", "https://ylilauta.org/pelit/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("h", "https://ylilauta.org/huumeet/", "navbar-button"));
-buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
-buttonContainer.appendChild(createButton("epic", "https://ylilauta.org/arkisto/", "navbar-button"));
+
+const boards = [...document.getElementById('nav-boards').getElementsByTagName("p")]
+    .map(board => board.getAttribute('data-board'))
+    .filter(board => !hidden.includes(board));
+
+for (var i = 0; i < boards.length; i++) {
+    const boardName = boards[i];
+    const abbreviation = boardAbbreviations[boardName] || boardName;
+    buttonContainer.appendChild(createButton(abbreviation, "https://ylilauta.org/" + boardName + "/", "navbar-button"));
+    if (i < boards.length - 1) {
+        buttonContainer.appendChild(createButton("/", "navbar-button", "merkki"));
+    }
+}
+
 buttonContainer.appendChild(createButton("]", "navbar-button", "merkki"));
+
+navbar.appendChild(buttonContainer);
 
 navbar.insertBefore(buttonContainer, navbar.children[1]);
 navbar.insertBefore(createButton("[", "navbar-button", "merkki"), navbar.children[3]);
